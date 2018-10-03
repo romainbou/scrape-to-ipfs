3123b674a26d87dde6d38a9ad892816331f916d8
Romain Bouyé <romain@bouye.fr>
2018-10-03 09:24:55 +0100

    Initial commit: Download a roughly scraped web page from passed URL in a file and serve the file

---
+++ README.md
@@ -0,0 +1,9 @@
+# Wikipedia to IPFS gateway
+
+Website to scape and index wikipedia pages and automatically push and serve them via IPFS
+
+## TODO
+
+[] Scrape a Wikipedia page on request and replace or save all relative URLs
+[] Push the files to an IPFS daemon
+[] Redirect to an IPFS gateway
---
+++ server.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+	//r.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	r.LoadHTMLGlob("templates/*")
+	// r.GET("/", func(c *gin.Context) {
+	// 	indexHandler(c)
+	// })
+
+	r.GET("/*trail", func(c *gin.Context) {
+		argHandler(c)
+	})
+
+	r.Run(":8080")
+}
+
+func indexHandler(c *gin.Context) {
+
+	URLParam := "url"
+
+	url := c.DefaultQuery(URLParam, "")
+
+	if isValidHTTPURL(url) {
+		srapeAndServe(url, c)
+	}
+
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title":    "Wikipedia Scaper",
+		"URLParam": URLParam,
+	})
+}
+
+func argHandler(c *gin.Context) {
+	url := c.Param("trail")
+	trimmedURL := url[1:]
+	log.Print("call is valid: ", trimmedURL)
+	if isValidHTTPURL(trimmedURL) {
+		log.Print("cqll scape qnd server", trimmedURL)
+		srapeAndServe(trimmedURL, c)
+	} else {
+		indexHandler(c)
+	}
+}
+
+func srapeAndServe(url string, c *gin.Context) {
+	log.Print("scrape")
+	filename := scrape(url)
+	log.Print("finished scrapting")
+	serveFile(filename, c)
+}
+
+func scrape(url string) string {
+	outputFilename := "output.html"
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	// Create output file
+	outFile, err := os.Create(outputFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outFile.Close()
+
+	// Copy data from HTTP response to file
+	_, err = io.Copy(outFile, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return outputFilename
+}
+
+func serveFile(filename string, c *gin.Context) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	}
+	reader := bytes.NewReader(file)
+	contentLength := int64(len(file))
+	contentType := "text/html;"
+
+	extraHeaders := map[string]string{}
+
+	c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
+
+}
+
+func isValidHTTPURL(rawURL string) bool {
+	URL, err := url.ParseRequestURI(rawURL)
+	if err != nil {
+		log.Print(err)
+		return false
+	} else if strings.HasPrefix(URL.Scheme, "http") {
+		return true
+	} else {
+		return false
+	}
+}
