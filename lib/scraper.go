0dc64e24744b7fcee7a34578ca47137db1a56322
Romain Bouyé <romain@bouye.fr>
2018-12-02 18:33:45 +0000

    New function to lookup the links in an HTML doc

--- lib/scraper.go
+++ lib/scraper.go
@@ -1,9 +1,13 @@
 package lib
 
 import (
+	"errors"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
 // Scrape downloads the content of the given URL and returns it as a string
@@ -34,5 +38,50 @@ func Scrape(url string) string {
 
 	bodyString := string(bodyBytes)
 
+	// Find asset (css, fonts, JS, images, videos, 3D files, iframe) URLS
+	// Add all of them to IPFS
+	// Replace them with the Gateway link
+
 	return bodyString
+}
+
+func getTokenLink(token html.Token) (linkValue string, err error) {
+	if token.Data == "a" || token.Data == "link" {
+		for _, a := range token.Attr {
+			if a.Key == "href" {
+				return a.Val, nil
+			}
+		}
+	}
+	if token.Data == "img" || token.Data == "script" {
+		for _, a := range token.Attr {
+			if a.Key == "src" {
+				return a.Val, nil
+			}
+		}
+	}
+	return "", errors.New("No value found in the link")
+}
+
+func findLinkedAsset(reader io.Reader) []string {
+
+	z := html.NewTokenizer(reader)
+
+	linksArray := []string{}
+
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			// End of doc
+			return linksArray
+		case html.StartTagToken:
+			t := z.Token()
+			linkValue, err := getTokenLink(t)
+			if err == nil {
+				// Link found
+				linksArray = append(linksArray, linkValue)
+			}
+		}
+	}
 }
--- lib/scraper_test.go
+++ lib/scraper_test.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -10,6 +11,31 @@ func TestScrape(t *testing.T) {
 	content := Scrape(helloURL)
 	if content != "hello worlds\n" {
 		t.Errorf("URL: %s should contains 'hello worlds\\n', received: %s", helloURL, content)
+	}
+
+}
+
+func TestFindAssetLinks(t *testing.T) {
+
+	html := `
+			<!DOCTYPE html>
+			<html lang="en-GB">
+				<head>
+					<link rel="stylesheet" href="https://golang.org/lib/godoc/style.css">
+					<script src="https://golang.org/lib/godoc/godocs.js" defer></script>
+				<head>
+				<body>
+					<a href="https://golang.org/doc">Documents</a>
+				</body>
+			</html>
+			`
+
+	reader := bytes.NewReader([]byte(html))
+
+	links := findLinkedAsset(reader)
+
+	if len(links) != 3 {
+		t.Errorf("The HTML should contains 3 links, %d received", len(links))
 	}
 
 }
